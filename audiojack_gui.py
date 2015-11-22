b589659353f415d92bdb8e3db8d765478399e5af
Gautam Mekkat <Blue9@users.noreply.github.com>
2015-11-22 18:55:02 -0500

    Allowed the program to be used multiple times

--- audiojack_gui.py
+++ audiojack_gui.py
@@ -23,12 +23,15 @@ class App:
     
     def search(self):
         results = audiojack.get_results(self.urlInput.get())
+        self.results_frame = Frame(self.frame)
         for i, result in enumerate(results):
             text = '%s - %s [from %s]' % (result[0], result[1], result[2])
-            self.result = Button(self.frame, text=text, command=partial(self.download, i))
+            self.result = Button(self.results_frame, text=text, command=partial(self.download, i))
             self.result.pack()
+        self.results_frame.pack()
     
     def download(self, index):
+        self.results_frame.destroy()
         file = audiojack.select(index)
         self.file = Button(self.frame, text=file, command=partial(self.openFile, file))
         self.file.pack()
