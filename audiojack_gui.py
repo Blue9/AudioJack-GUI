770b972a69aa58abc71514d5c1bdf9a04b480f87
Gautam Mekkat <Blue9@users.noreply.github.com>
2015-11-22 20:12:29 -0500

    Added cover art preview

--- audiojack_gui.py
+++ audiojack_gui.py
@@ -1,7 +1,10 @@
-from Tkinter import *
-from functools import partial
-import audiojack
 import os
+from functools import partial
+
+from Tkinter import *
+from PIL import Image, ImageTk
+from cStringIO import StringIO
+import audiojack
 
 #audiojack.set_useragent('name', 'version') MUST SET THIS
 
@@ -22,16 +25,37 @@ class App:
         self.submit.pack()
     
     def search(self):
-        results = audiojack.get_results(self.urlInput.get())
+        self.results = audiojack.get_results(self.urlInput.get())
         self.results_frame = Frame(self.frame)
-        for i, result in enumerate(results):
+        for i, result in enumerate(self.results):
             text = '%s - %s [from %s]' % (result[0], result[1], result[2])
             self.result = Button(self.results_frame, text=text, command=partial(self.download, i))
+            self.result.bind('<Enter>', partial(self.showImage, i))
+            self.result.bind('<Leave>', self.hideImage)
             self.result.pack()
         self.results_frame.pack()
     
+    def showImage(self, index, event):
+        self.image_frame = Frame(self.frame)
+        self.image_frame.config(height=200)
+        self.image_frame.pack()
+        
+        image_data = Image.open(StringIO(audiojack.get_cover_art_as_data(self.results[index][3]).decode('base64')))
+        image_data = image_data.resize((200,200), Image.ANTIALIAS)
+        self.image_tk = ImageTk.PhotoImage(image=image_data)
+        self.image = Label(self.image_frame, image=self.image_tk, height=200, width=200, background='#000')
+        self.image.pack()
+    
+    def hideImage(self, event):
+        self.image_frame.pack_forget()
+        self.image.pack_forget()
+        self.image.destroy()
+    
     def download(self, index):
+        self.results_frame.pack_forget()
         self.results_frame.destroy()
+        self.image_frame.pack_forget()
+        self.image_frame.destroy()
         file = audiojack.select(index)
         self.file = Button(self.frame, text=file, command=partial(self.openFile, file))
         self.file.pack()
