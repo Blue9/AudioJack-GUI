50ddd4a329af76ef2c4ee83e09f1325619f7632d
Gautam Mekkat <Blue9@users.noreply.github.com>
2015-11-23 20:03:28 -0500

    Major update to styling and format

--- audiojack_gui.py
+++ audiojack_gui.py
@@ -10,54 +10,87 @@ audiojack.set_useragent('AudioJack-GUI', '0.1')
 
 class AudioJackGUI(object):
     def __init__(self, master):
-        self.frame = Frame(master)
-        self.frame.pack()
+        master.minsize(width=1280, height=720)
+        self.frame = Frame(master, bg='#2c3c50')
+        self.frame.pack(fill=BOTH, expand=1)
         
-        self.title = Label(self.frame, text='AudioJack')
+        self.title = Label(self.frame, text='AudioJack', fg='#fff', bg='#2c3c50')
         self.title.pack()
         
-        self.url = Label(self.frame, text='Enter Youtube or SoundCloud URL: ')
+        self.url = Label(self.frame, text='Enter Youtube or SoundCloud URL: ', fg='#fff', bg='#2c3c50')
         self.url.pack()
-        self.urlInput = Entry(self.frame)
-        self.urlInput.pack()
+        self.url_input = Entry(self.frame)
+        self.url_input.pack()
 
-        self.submit = Button(self.frame, text='Go!', command=self.search)
+        self.submit = Button(self.frame, text='Go!', fg='#fff', bg='#20648f', command=self.search)
         self.submit.pack()
     
-    def search(self):
-        self.results = audiojack.get_results(self.urlInput.get())
-        self.results_frame = Frame(self.frame)
-        for i, result in enumerate(self.results):
-            text = '%s - %s [from %s]' % (result[0], result[1], result[2])
-            self.result = Button(self.results_frame, text=text, command=partial(self.download, i))
-            self.result.bind('<Enter>', partial(self.showImage, i))
-            self.result.bind('<Leave>', self.hideImage)
-            self.result.pack()
-        self.results_frame.pack()
-    
-    def showImage(self, index, event):
-        self.image_frame = Frame(self.frame)
-        self.image_frame.config(height=200)
-        self.image_frame.pack()
+    def reset(self):
+        try:
+            self.results_frame.pack_forget()
+            self.results_frame.destroy()
+        except Exception:
+            pass
         
-        image_data = Image.open(StringIO(audiojack.get_cover_art_as_data(self.results[index][3]).decode('base64')))
-        image_data = image_data.resize((200,200), Image.ANTIALIAS)
-        self.image_tk = ImageTk.PhotoImage(image=image_data)
-        self.image = Label(self.image_frame, image=self.image_tk, height=200, width=200, background='#000')
-        self.image.pack()
+        try:
+            self.custom_frame.pack_forget()
+            self.custom_frame.destroy()
+        except Exception:
+            pass
+        
+        try:
+            self.file.pack_forget()
+            self.file.destroy()
+        except Exception:
+            pass
     
-    def hideImage(self, event):
-        self.image_frame.pack_forget()
-        self.image.pack_forget()
-        self.image.destroy()
+    def search(self):
+        self.reset()
+        self.results = audiojack.get_results(self.url_input.get())
+        self.results_frame = Frame(self.frame, bg='#2c3c50')
+        self.image_tk = []
+        for i, result in enumerate(self.results[:8]):
+            image_data = Image.open(StringIO(audiojack.get_cover_art_as_data(self.results[i][3]).decode('base64')))
+            image_data = image_data.resize((200, 200), Image.ANTIALIAS)
+            self.image_tk.append(ImageTk.PhotoImage(image=image_data))
+        for i, result in enumerate(self.results[:8]):
+            text = '%s\n%s\n%s' % (result[0], result[1], result[2])
+            self.result = Button(self.results_frame, text=text, fg='#fff', bg='#444', image=self.image_tk[i], compound=TOP, command=partial(self.download, i))
+            self.result.grid(column=i%4, row=i/4)
+        self.results_frame.pack()
+        
+        self.custom_frame = Frame(self.frame, bg='#2c3c50')
+        self.artist_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Artist: ')
+        self.artist_input = Entry(self.custom_frame, fg='#000')
+        self.title_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Title: ')
+        self.title_input = Entry(self.custom_frame, fg='#000')
+        self.album_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Album: ')
+        self.album_input = Entry(self.custom_frame, fg='#000')
+        self.custom_submit = Button(self.custom_frame, text='Download using custom tags', fg='#fff', bg='#20648f', command=self.custom)
+        self.artist_label.grid(column=0, row=0)
+        self.artist_input.grid(column=1, row=0)
+        self.title_label.grid(column=0, row=1)
+        self.title_input.grid(column=1, row=1)
+        self.album_label.grid(column=0, row=2)
+        self.album_input.grid(column=1, row=2)
+        self.custom_submit.grid(row=3, columnspan=2)
+        self.custom_frame.pack(pady=20)
     
     def download(self, index):
-        self.results_frame.pack_forget()
-        self.results_frame.destroy()
-        self.image_frame.pack_forget()
-        self.image_frame.destroy()
+        self.reset()
         file = audiojack.select(index)
         self.file = Button(self.frame, text=file, command=partial(self.openFile, file))
+        self.file.pack()
+    
+    def custom(self):
+        artist = self.artist_input.get()
+        title = self.title_input.get()
+        album = self.album_input.get()
+        self.reset()
+        file = audiojack.custom(artist, title, album)
+        self.file = Button(self.frame, text=file, command=partial(self.openFile, file))
+        if file == '':
+            file == 'Song.mp3'
         self.file.pack()
     
     def openFile(self, file):
