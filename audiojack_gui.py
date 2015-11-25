e2ad34641482cddd1ec14b20f0646c077f4ca184
Gautam Mekkat <Blue9@users.noreply.github.com>
2015-11-24 19:43:09 -0500

    Improved GUI appearance

--- audiojack_gui.py
+++ audiojack_gui.py
@@ -13,16 +13,18 @@ class AudioJackGUI(object):
         master.minsize(width=1280, height=720)
         self.frame = Frame(master, bg='#2c3c50')
         self.frame.pack(fill=BOTH, expand=1)
+
+        self.font = ('Segoe UI', 10)
         
-        self.title = Label(self.frame, text='AudioJack', fg='#fff', bg='#2c3c50')
+        self.title = Label(self.frame, text='AudioJack', font=self.font, fg='#fff', bg='#2c3c50')
         self.title.pack()
         
-        self.url = Label(self.frame, text='Enter Youtube or SoundCloud URL: ', fg='#fff', bg='#2c3c50')
+        self.url = Label(self.frame, text='Enter YouTube or SoundCloud URL', font=self.font, fg='#fff', bg='#2c3c50')
         self.url.pack()
         self.url_input = Entry(self.frame)
         self.url_input.pack()
 
-        self.submit = Button(self.frame, text='Go!', fg='#fff', bg='#20648f', activeforeground='#fff', activebackground='#1a5274', command=self.search)
+        self.submit = Button(self.frame, text='Go!', font=self.font, fg='#fff', bg='#20648f', activeforeground='#fff', activebackground='#1a5274', command=self.search)
         self.submit.pack()
     
     def reset(self):
@@ -54,7 +56,7 @@ class AudioJackGUI(object):
         self.reset()
         self.results = audiojack.get_results(self.url_input.get())
         self.results_frame = Frame(self.frame, bg='#2c3c50')
-        self.results_label = Label(self.frame, text='Results:', bg='#2c3c50', fg='#fff')
+        self.results_label = Label(self.frame, text='Results:', font=self.font, bg='#2c3c50', fg='#fff')
         self.results_label.pack()
         self.image_tk = []
         for i, result in enumerate(self.results[:8]):
@@ -63,7 +65,7 @@ class AudioJackGUI(object):
             self.image_tk.append(ImageTk.PhotoImage(image=image_data))
         for i, result in enumerate(self.results[:8]):
             text = '%s\n%s\n%s' % (result[0], result[1], result[2])
-            self.result = Button(self.results_frame, text=text, fg='#fff', bg='#444', activeforeground='#fff', activebackground='#222', image=self.image_tk[i], highlightbackground='#444', highlightthickness=4, compound=TOP, command=partial(self.download, i))
+            self.result = Button(self.results_frame, text=text, font=self.font, fg='#fff', bg='#444', activeforeground='#fff', activebackground='#222', image=self.image_tk[i], highlightbackground='#444', highlightthickness=4, compound=TOP, command=partial(self.download, i))
             self.result.bind('<Enter>', partial(self.result_hover, self.result))
             self.result.bind('<Leave>', partial(self.result_leave, self.result))
             self.result.grid(column=i%4, row=i/4)
@@ -78,21 +80,23 @@ class AudioJackGUI(object):
     
     def create_custom_frame(self):
         self.custom_frame = Frame(self.frame, bg='#2c3c50')
-        self.artist_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Artist: ')
-        self.artist_input = Entry(self.custom_frame, fg='#000')
-        self.title_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Title: ')
-        self.title_input = Entry(self.custom_frame, fg='#000')
-        self.album_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Album: ')
-        self.album_input = Entry(self.custom_frame, fg='#000')
-        self.custom_submit = Button(self.custom_frame, text='Download using custom tags', fg='#fff', bg='#20648f', activeforeground='#fff', activebackground='#1a5274', command=self.custom)
-        self.artist_label.grid(column=0, row=0)
-        self.artist_input.grid(column=1, row=0)
-        self.title_label.grid(column=0, row=1)
-        self.title_input.grid(column=1, row=1)
-        self.album_label.grid(column=0, row=2)
-        self.album_input.grid(column=1, row=2)
-        self.custom_submit.grid(row=3, columnspan=2)
-        self.custom_frame.pack(pady=20)
+        self.custom_title = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Custom tags:', font=self.font)
+        self.artist_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Artist: ', font=self.font)
+        self.artist_input = Entry(self.custom_frame, fg='#000', font=self.font)
+        self.title_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Title: ', font=self.font)
+        self.title_input = Entry(self.custom_frame, fg='#000', font=self.font)
+        self.album_label = Label(self.custom_frame, fg='#fff', bg='#2c3c50', text='Album: ', font=self.font)
+        self.album_input = Entry(self.custom_frame, fg='#000', font=self.font)
+        self.custom_submit = Button(self.custom_frame, text='Download using custom tags', font=self.font, fg='#fff', bg='#20648f', activeforeground='#fff', activebackground='#1a5274', command=self.custom)
+        self.custom_title.grid(row=0, columnspan=2)
+        self.artist_label.grid(column=0, row=1)
+        self.artist_input.grid(column=1, row=1)
+        self.title_label.grid(column=0, row=2)
+        self.title_input.grid(column=1, row=2)
+        self.album_label.grid(column=0, row=3)
+        self.album_input.grid(column=1, row=3)
+        self.custom_submit.grid(row=4, columnspan=2, sticky=EW, pady=10)
+        self.custom_frame.pack(pady=10)
     
     def download(self, index):
         self.reset()
@@ -106,7 +110,7 @@ class AudioJackGUI(object):
         album = self.album_input.get()
         self.reset()
         file = audiojack.custom(artist, title, album)
-        self.file = Button(self.frame, text=file, command=partial(self.openFile, file))
+        self.file = Button(self.frame, text=file, font=self.font, command=partial(self.openFile, file))
         if file == '':
             file == 'Song.mp3'
         self.file.pack()
