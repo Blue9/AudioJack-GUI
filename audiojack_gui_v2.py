2dfb4e9b27dfe88468b52afd4043f3ac159bff39
Gautam M <Blue9@users.noreply.github.com>
2015-12-07 18:53:18 -0500

    Use multithreading to prevent freezing

--- audiojack_gui_v2.py
+++ audiojack_gui_v2.py
@@ -1,6 +1,9 @@
 import os
 from functools import partial
 
+from threading import Thread
+import Queue
+
 from Tkinter import *
 import ttk
 from PIL import Image, ImageTk
@@ -11,16 +14,17 @@ audiojack.set_useragent('AudioJack-GUI v2', '0.1')
 
 class AudioJackGUI_v2(object):
     def __init__(self, master):
+        self.master = master
         self.font = ('Segoe UI', 10)
         
-        master.minsize(width=1280, height=720)
-        self.mainframe = ttk.Frame(master)
+        self.master.minsize(width=1280, height=720)
+        self.mainframe = ttk.Frame(self.master)
         self.mainframe.pack()
         
         self.title = ttk.Label(self.mainframe, text='AudioJack', font=('Segoe UI Light', 24))
         self.title.pack()
         
-        self.url_input = Text(self.mainframe, width=41, height=1, font=self.font, wrap=NONE)
+        self.url_input = Text(self.mainframe, width=40, height=1, font=self.font, wrap=NONE)
         self.url_input.bind('<Return>', self.search)
         self.url_input.pack()
         
@@ -54,24 +58,39 @@ class AudioJackGUI_v2(object):
         except Exception:
             pass
     
+    def get_results(self, input):
+        results = audiojack.get_results(input)
+        images = []
+        for i, result in enumerate(results[:8]):
+            image_data = Image.open(StringIO(audiojack.get_cover_art_as_data(results[i][3]).decode('base64')))
+            image_data = image_data.resize((200, 200), Image.ANTIALIAS)
+            images.append(ImageTk.PhotoImage(image=image_data))
+        self.q.put([results, images])
+    
     def search(self, event=None):
         input = self.url_input.get(0.0, END).replace('\n', '')
         self.reset()
-        self.results = audiojack.get_results(input)
-        self.results_frame = ttk.Frame(self.mainframe)
-        self.results_label = ttk.Label(self.mainframe, text='Results:', font=self.font)
-        self.results_label.pack()
-        self.image_tk = []
-        for i, result in enumerate(self.results[:8]):
-            image_data = Image.open(StringIO(audiojack.get_cover_art_as_data(self.results[i][3]).decode('base64')))
-            image_data = image_data.resize((200, 200), Image.ANTIALIAS)
-            self.image_tk.append(ImageTk.PhotoImage(image=image_data))
-        for i, result in enumerate(self.results[:8]):
-            text = '%s\n%s\n%s' % (result[0], result[1], result[2])
-            self.result = ttk.Button(self.results_frame, text=text, image=self.image_tk[i], compound=TOP, command=partial(self.download, i))
-            self.result.grid(column=i%4, row=i/4)
-        self.results_frame.pack()
-        self.create_custom_frame()
+        self.q = Queue.Queue()
+        t = Thread(target=self.get_results, args=[input])
+        t.start()
+        self.master.after(100, self.add_results)
+    
+    def add_results(self):
+        try:
+            self.results_images = self.q.get(0)
+            self.results = self.results_images[0]
+            self.images = self.results_images[1]
+            self.results_frame = ttk.Frame(self.mainframe)
+            self.results_label = ttk.Label(self.mainframe, text='Results:', font=self.font)
+            self.results_label.pack()
+            for i, result in enumerate(self.results[:8]):
+                text = '%s\n%s\n%s' % (result[0], result[1], result[2])
+                self.result = ttk.Button(self.results_frame, text=text, image=self.images[i], compound=TOP, command=partial(self.download, i))
+                self.result.grid(column=i%4, row=i/4)
+            self.results_frame.pack()
+            self.create_custom_frame()
+        except Queue.Empty:
+            self.master.after(100, self.add_results)
     
     def create_custom_frame(self):
         self.custom_frame = ttk.Frame(self.mainframe)
@@ -93,16 +112,29 @@ class AudioJackGUI_v2(object):
         self.custom_submit.grid(row=4, columnspan=2, sticky=EW, pady=10)
         self.custom_frame.pack(pady=10)
     
+    def get_file(self, index, download_queue):
+        file = audiojack.select(index)
+        download_queue.put(file)
+    
     def download(self, index):
         self.reset()
-        file = audiojack.select(index)
-        text = 'Open %s' % file
-        self.file = ttk.Button(self.mainframe, text=text, command=partial(self.open_file, file))
-        self.results_label.pack_forget()
-        self.results_label.destroy()
-        self.results_frame.pack_forget()
-        self.results_frame.destroy()
-        self.file.pack()
+        self.download_queue = Queue.Queue()
+        t = Thread(target=self.get_file, args=[index, self.download_queue])
+        t.start()
+        self.master.after(100, self.add_file)
+    
+    def add_file(self):
+        try:
+            self.file = self.download_queue.get(0)
+            text = 'Open %s' % self.file
+            self.file_button = ttk.Button(self.mainframe, text=text, command=partial(self.open_file, self.file))
+            self.results_label.pack_forget()
+            self.results_label.destroy()
+            self.results_frame.pack_forget()
+            self.results_frame.destroy()
+            self.file_button.pack()
+        except Queue.Empty:
+            self.master.after(100, self.add_file)
     
     def custom(self):
         artist = self.artist_input.get(0.0, END).replace('\n', '')
